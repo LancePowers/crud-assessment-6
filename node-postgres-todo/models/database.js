34496328b63c989467cdee78b95507b62a81cafb
Lance <Lance@thelpstory.com>
2015-11-10 19:41:03 -0700

    schedule post route complete

--- node-postgres-todo/models/database.js
+++ node-postgres-todo/models/database.js
@@ -1,8 +1,10 @@
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
-var client = new pg.client(connectionString);
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/vegan';
+var client = new pg.Client(connectionString);
 client.connect();
-var query = clieng.query('CREAT TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)')
+
+var query = client.query('CREATE TABLE schedules(id SERIAL PRIMARY KEY, user_id VARCHAR(30) NOT NULL, user_schedule BIT(7) NOT NULL)')
 query.on('end', function () {
+    console.log('schedules table created')
     client.end();
 });
--- node-postgres-todo/routes/index.js
+++ node-postgres-todo/routes/index.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+var connectionString = 'postgres://localhost:5432/vegan';
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+/* Create a single vegan. */
+router.post('/api/v1/schedules', function (req, res) {
+    console.log(req.body)
+    var results = [];
+
+    //Grab data from http request
+    var data = {
+        user: req.body.user,
+        schedule: req.body.schedule
+    };
+
+
+    //Get client from connection pool
+    pg.connect(connectionString, function (err, client, done) {
+        //Connection error handling
+        if (err) {
+            done();
+            console.log(err);
+            return res.status(500).json({
+                success: false,
+                data: err
+            });
+        }
+
+        //SQL Query > insert data
+        client.query("INSERT INTO schedules(user_id, user_schedule) values($1, $2)", [data.user, data.schedule]);
+
+        //SQL Query > select data
+        var query = client.query("SELECT * FROM schedules ORDER BY id ASC");
+
+        //Stream results back one row at a time
+        query.on('row', function (row) {
+            console.log('here');
+            results.push(row);
+        });
+
+        //Stream results back one row at a time
+        query.on('end', function () {
+            done();
+            return res.json(results);
+        });
+
+    });
 });
 
 module.exports = router;
